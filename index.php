24c2456f49b49ebd53ca8b61cee729cc1e226391
sollendy <edlannino@tutanota.com>
2023-04-19 16:42:56 +0200

    difficoltà ad andare avanti

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    //variabili
+    $passwLenght = $_GET["lunghezza"];
+    function $generaPassw() {
+        $minus = ('abcdefghilmnopqrstuvz')
+    }
+    //
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>generatore passw</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+    <div class="form-cnt w-75 m-auto text-center">
+        <h1>form per password</h1>
+    <form action="#">
+        <label for="password">Lunghezza password: </label>
+        <input name="lunghezza" type="number">
+    </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
